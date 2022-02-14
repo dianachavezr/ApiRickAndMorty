7f03b4fefcd69550cf7559d915506fea6794b221
iElectro <ejose.acc@gmail.com>
2022-02-14 15:47:16 -0500

    Importada la fuente

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import Copyright from './copyright/Copyright'
 const TestComponent = () => {
   return (
     <div className='TestComponent'>
+      <Copyright />
     </div>
   )
 }
---
+++ src/components/copyright/Copyright.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Copyright = () => {
+  return (
+    <span>Copyright 2022. All Rights Reserved</span>
+  )
+}
+
+export default Copyright
