9fc637ce55f1d926699d3644634ffa68e64748c0
carolo180 <caponeris1@gmail.com>
2022-02-21 18:13:21 -0500

    Upload styling improves

---
+++ src/components/TestComponent.js
@@ -0,0 +1,13 @@
+import LandingSec from "./Sections/LandingSec/LandinSec";
+
+
+    function TestComponent(){
+
+        return (
+            <div className="testComponent">
+                <LandingSec />
+           
+            </div>
+          );
+      } 
+    export default TestComponent;
