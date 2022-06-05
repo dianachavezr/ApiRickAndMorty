f8210e782653ba713f2aebe0ab5e9d0516a0f386
DIANACH <dianepatrich@gmail.com>
2022-06-05 15:45:03 -0500

    .

--- src/components/Buttons/closeButton/CloseButton.jsx
+++ src/components/Buttons/closeButton/CloseButton.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import CloseBtn from './closeButton.css'
-import CloseBtn1 from '../../../assets/close.png'
+import React from "react";
+import CloseBtn from "./closeButton.css";
+import CloseBtn1 from "../../../assets/close.png";
 
-function CloseButton({background, text, color}) {
-        {/* CLOSE BUTTON PARA EL MODAL */}
+function CloseButton({ background, text, color }) {
+  {
+    /* CLOSE BUTTON PARA EL MODAL*/
+  }
 
   return (
     <div>
-    <button className='closeButton'><img alt="closeBtn" src= {CloseBtn1}/> </button>
-    </div>  
-  )
+      <button className="closeButton">
+        <img alt="closeBtn" src={CloseBtn1} />{" "}
+      </button>
+    </div>
+  );
 }
-export default CloseButton
+export default CloseButton;
